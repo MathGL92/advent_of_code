e7a48f164710f5da22bbaae8c909d865c9b6f504
Mathieu Longe <mathieu.longe@orange.fr>
2020-12-05 17:08:03 +1100

    day 5 done

---
+++ day_5/day_5_part2.rb
@@ -0,0 +1,48 @@
+
+require 'pry'
+
+input = File.open("/Users/mathieulonge/code/MathGL92/advent_of_code/day_5/input.txt").read
+
+# FBFBBFFRLR : 44 5 357
+def calculate_id(seat)
+  first_row = 0
+  last_row = 127
+  rows = (first_row..last_row).to_a
+  i = 0
+  until rows.size == 1
+    mid_row = first_row + ((last_row - first_row) / 2)
+    if seat[i] == "F"
+      last_row = mid_row
+    elsif seat[i] == "B"
+      first_row = mid_row + 1
+    end
+    rows = (first_row..last_row).to_a
+    i += 1
+  end
+  rows
+  # find the column
+  first_col = 0
+  last_col = 7
+  cols = (first_col..last_col).to_a
+  i = 7
+  until cols.size == 1
+    mid_col = first_col + ((last_col - first_col) / 2)
+    if seat[i] == "L"
+      last_col = mid_col
+    elsif seat[i] == "R"
+      first_col = mid_col + 1
+    end
+    cols = (first_col..last_col).to_a
+    i += 1
+  end
+  cols
+  # calculate the seat id
+  rows.first * 8 + cols.first
+end
+
+def my_seat(input)
+  array_seats = input.split("\n")
+  (40..980).to_a - array_seats.map {|seat| calculate_id(seat) }
+end
+
+p my_seat(input)
