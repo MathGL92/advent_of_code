6d53ae2fec5ef46fd3832b0fec575a6d46c66bae
Mathieu Longe <mathieu.longe@orange.fr>
2020-12-07 11:57:13 +1100

    day 6 done

--- day_6/day_6_part2.rb
+++ day_6/day_6_part2.rb
@@ -1,7 +1,12 @@
 input = File.open("/Users/mathieulonge/code/MathGL92/advent_of_code/day_6/input.txt").read
 
+example = File.open("/Users/mathieulonge/code/MathGL92/advent_of_code/day_6/example.txt").read
+
 def count_yes(string)
-  string.split("\n\n").map { |group| group.gsub("\n", '') }.map { |group| group.chars }.map { |group| group.uniq }.map { |group| group.length }.sum
+  groups = string.split("\n\n").map { |group| group.split("\n").map { |person| person.chars }.reduce { |a, b| a & b }.map(&:size) }.flatten.sum
+  # for each group asnwers, keep the common ones
+
+
 end
 
 p count_yes(input)
